08ecfdbcccb97af6afee1c7360c5dee70b656910
WilliamSampson44444 <42818523+WilliamSampson44444@users.noreply.github.com>
2021-06-13 14:15:48 -0700

    finish KNN

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import numpy as np
 import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import linear_model, preprocessing
 import matplotlib.pyplot as pyplot
 import pickle
 from matplotlib import style
